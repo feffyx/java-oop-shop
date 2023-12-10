611a750c31aa0ba1d55e953dedee1e2e5cf37825
feffyx <ziacofederica@gmail.com>
2023-12-10 17:52:01 +0100

    Completed Assignment.

--- org/lessons/java/shop/Main.java
+++ org/lessons/java/shop/Main.java
@@ -3,22 +3,22 @@ package org.lessons.java.shop;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Creazione di un prodotto con dati validi
+
             Prodotto prodottoValido = new Prodotto("Laptop", "Powerful laptop", 800.0, 22.0);
 
-            // Creazione di un prodotto con prezzo negativo (genererà un'eccezione)
 
-            // Utilizzo delle variabili per evitare warning
+
+
             System.out.println("Dettagli Prodotto Valido:");
             displayProductDetails(prodottoValido);
 
-            // Non stiamo utilizzando prodottoPrezzoNegativo in questo esempio
+
         } catch (IllegalArgumentException e) {
             System.out.println("Errore durante la creazione del prodotto: " + e.getMessage());
         }
     }
 
-    // Utility method to display product details
+
     private static void displayProductDetails(Prodotto prodotto) {
         System.out.println("Codice: " + prodotto.getCodice());
         System.out.println("Nome: " + prodotto.getNome());
--- org/lessons/java/shop/Prodotto.java
+++ org/lessons/java/shop/Prodotto.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Prodotto {
     private final int codice;
-    private String nome;
+    private final String nome;
     private final String descrizione;
-    private double prezzo;
-    private double iva;
+    private final double prezzo;
+    private final double iva;
 
     public Prodotto(String nome, String descrizione, double prezzo, double iva) {
         if (prezzo < 0) {
@@ -35,9 +35,6 @@ public class Prodotto {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public String getDescrizione() {
         return descrizione;
@@ -47,29 +44,21 @@ public class Prodotto {
         return prezzo;
     }
 
-    public void setPrezzo(double prezzo) {
-        this.prezzo = prezzo;
-    }
 
 
     public double getIva() {
         return iva;
     }
 
-    public void setIva(double iva) {
-        this.iva = iva;
-    }
 
 
     public double getPrezzoConIva() {
         return prezzo + (prezzo * iva / 100);
     }
 
-
     public String getNomeEsteso() {
         return codice + "-" + nome;
     }
-
 
     private int generateRandomCode() {
         return new Random().nextInt(1000) + 1;
